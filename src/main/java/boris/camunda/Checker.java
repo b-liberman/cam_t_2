f7d16ff31f28a708c19e0e97afb32d9e819d2b70
Boris Liberman <myemail@321.de>
2019-07-15 14:32:31 +0200

    commit

--- src/main/java/boris/camunda/Checker.java
+++ src/main/java/boris/camunda/Checker.java
@@ -1,16 +1,21 @@
 package boris.camunda;
 
+import java.io.File;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class Checker {
+
+	@Autowired
+	private File myFile;
 	
 	public void printName(DelegateExecution execution) {
 		log.info("execution123: {}", execution.getCurrentActivityName());
+		myFile.toPath();  
 	}
 
 }
